24412e0e40f2ebc721d7a75c33adb4b8c150bc29
Nishad94 <nishad.dawkhar94@gmail.com>
2013-08-10 14:14:06 +0530

    Updated

--- Problem 1/1A/main.c
+++ Problem 1/1A/main.c
@@ -3,7 +3,7 @@
 #include<time.h>
 #define NUM1 3
 #define NUM2 5          // Sum of multiples of 3 and 5 are to be calculated below 1000
-#define LIMIT 1000000000
+#define LIMIT 1000
 
 /*
 Project Euler Problem 1
@@ -14,12 +14,12 @@ int main()
 {
     long long Total = 0;
     int i;
-    clock_t start = clock();
+    //clock_t start = clock();
     for(i = 1; i < LIMIT; i++)
         if(((i % NUM1) == 0) || ((i % NUM2) == 0))
              Total += i;
-    clock_t end = clock();
-    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-    printf("%lld %f",Total,seconds);
+    //clock_t end = clock();
+    //float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("%lld",Total);
     return 0;
 }
